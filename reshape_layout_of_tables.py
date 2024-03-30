aafc35d66ff1479ce78177dac2d9e2050fc5446d
salman <sf991804@gmail.com>
2024-03-30 11:14:31 +0500

    added air_quality_long.csv file

---
+++ reshape_layout_of_tables.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+titanic = pd.read_csv("titanic.csv")
+print(titanic.head())
+
+air_quality = pd.read_csv("air_quality_long.csv", index_col="date.utc", parse_dates=True)
+print(air_quality)
+
+
+#Now we reshape the layout of Tables
+# First we sort table rows
+print(titanic.sort_values(by="Age").head())
+
+#Now I want to sort the Titanic data according to the cabin class and age in descending order.
+print(titanic.sort_values(by=['Pclass', 'Age'], ascending=False).head())
